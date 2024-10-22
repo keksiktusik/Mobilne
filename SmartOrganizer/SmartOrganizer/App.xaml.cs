333363c26e94d66ddc5ae3297e07fba7f5d34f0e
Marta <martaprzyszowa@gmail.com>
2024-10-22 10:21:47 +0200

    dodanie kolorów

---
+++ SmartOrganizer/SmartOrganizer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartOrganizer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SmartOrganizer/SmartOrganizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartOrganizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
